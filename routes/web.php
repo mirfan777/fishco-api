5253ea4f907bd87cf7c65344dd6824fbccef592a
mirfan777 <fcmirfan122@gmail.com>
2024-10-22 23:06:10 +0700

    buat struktur file pages

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.dashboard.index');
-});
+Route::get('/', function () { return view('pages.dashboard.index'); });
+Route::get('/dashboard', function () { return view('pages.dashboard.index'); });
 
-Route::get('/fish', function () {
-    return view('pages.fish.index');
-});
+// Fish routes
+Route::get('/fish/disease', function () { return view('pages.fish.disease.index'); });
+Route::get('/fish/disease/detail', function ($id) { return view('pages.fish.disease.detail');});
+Route::get('/fish', function () { return view('pages.fish.fish.index'); });
+Route::get('/fish/detail', function ($id) { return view('pages.fish.fish.detail'); });
 
-Route::get('/fishe', function () {
-    return view('pages.fish.test');
-});
+// Medicine routes
+Route::get('/medicine', function () { return view('pages.medicine.medicine.index'); });
+Route::get('/medicine/detail', function ($id) { return view('pages.medicine.medicine.detail');});
+
+// Product routes
+Route::get('/product/affiliate', function () { return view('pages.product.affiliate.index'); });
+Route::get('/product', function () { return view('pages.product.product.index'); });
+
+// Report routes
+Route::get('/report', function () { return view('pages.report.index'); });
+
+// User routes
+Route::get('/user', function () { return view('pages.user.index'); });
 
 require __DIR__.'/auth.php';
