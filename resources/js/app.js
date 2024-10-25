53b64933bcf4a64a7e6f851535952822361033e9
mirfan777 <fcmirfan122@gmail.com>
2024-10-25 23:40:50 +0700

    add jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,9 @@ import "../css/style.css";
 import Alpine from "alpinejs";
 import persist from "@alpinejs/persist";
 import 'flowbite';
+import jQuery from 'jquery';
 
+window.$ = jQuery;
 Alpine.plugin(persist);
 window.Alpine = Alpine;
 Alpine.start();
