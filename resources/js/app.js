280c1bf333f2c3ecece874de72063f22311ff5f7
zollahrp <zollaperdana29@gmail.com>
2024-10-29 22:19:36 +0700

    Landing Page v.1.1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,24 @@
-import "../css/style.css";
-
-import Alpine from "alpinejs";
-import persist from "@alpinejs/persist";
-import 'flowbite';
-import jQuery from 'jquery';
-
-window.$ = jQuery;
-Alpine.plugin(persist);
-window.Alpine = Alpine;
-Alpine.start();
-
+// Import CSS
 import "flatpickr/dist/flatpickr.min.css";
 import "../css/satoshi.css";
 import "../css/style.css";
 
+// Import Libraries
 import Alpine from "alpinejs";
 import persist from "@alpinejs/persist";
 import flatpickr from "flatpickr";
-import chart01 from "./components/chart-01";
-import chart02 from "./components/chart-02";
-import chart03 from "./components/chart-03";
-import chart04 from "./components/chart-04";
+import 'flowbite';
+import jQuery from 'jquery';
 
+// Make jQuery globally available
+window.$ = jQuery;
+
+// Alpine.js Initialization
 Alpine.plugin(persist);
 window.Alpine = Alpine;
 Alpine.start();
 
-// Init flatpickr
+// Initialize flatpickr with options
 flatpickr(".datepicker", {
   mode: "range",
   static: true,
@@ -38,17 +30,16 @@ flatpickr(".datepicker", {
   nextArrow:
     '<svg class="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M1.4 10.8L0 9.4l4-4-4-4L1.4 0l5.4 5.4z" /></svg>',
   onReady: (selectedDates, dateStr, instance) => {
-    // eslint-disable-next-line no-param-reassign
     instance.element.value = dateStr.replace("to", "-");
     const customClass = instance.element.getAttribute("data-class");
     instance.calendarContainer.classList.add(customClass);
   },
   onChange: (selectedDates, dateStr, instance) => {
-    // eslint-disable-next-line no-param-reassign
     instance.element.value = dateStr.replace("to", "-");
   },
 });
 
+// Initialize flatpickr for form-datepicker
 flatpickr(".form-datepicker", {
   mode: "single",
   static: true,
@@ -60,7 +51,13 @@ flatpickr(".form-datepicker", {
     '<svg class="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M1.4 10.8L0 9.4l4-4-4-4L1.4 0l5.4 5.4z" /></svg>',
 });
 
-// Document Loaded
+// Import and Initialize Charts
+import chart01 from "./components/chart-01";
+import chart02 from "./components/chart-02";
+import chart03 from "./components/chart-03";
+import chart04 from "./components/chart-04";
+
+// Document Loaded Event
 document.addEventListener("DOMContentLoaded", () => {
   chart01();
   chart02();
