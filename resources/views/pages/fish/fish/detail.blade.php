27730274bfc54e9d87bf109e88dc10c3d94910dc
mirfan777 <fcmirfan122@gmail.com>
2024-10-30 11:58:29 +0700

    nitip

--- resources/views/pages/fish/fish/detail.blade.php
+++ resources/views/pages/fish/fish/detail.blade.php
@@ -13,7 +13,7 @@
 <body>
     @include('pages.fish.fish.modals.upload-image')
     @include('pages.fish.fish.modals.edit')
-    @include('pages.fish.fish.modals.delete')
+    @include('pages.fish.fish.modals.delete-image')
     @include('layout.main')
     
     <main class="sm:ml-64 min-h-screen mt-20 bg-gray-50">
@@ -53,7 +53,6 @@
                 <div class="flex md:flex-row flex-col w-full gap-5 mt-5">
                     <button data-modal-target="upload-image" data-modal-toggle="upload-image" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah data foto</button>
                     <button data-modal-target="edit-fish" data-modal-toggle="edit-fish" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Edit data</button>
-                    <button data-modal-target="delete-fish" data-modal-toggle="delete-fish" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Hapus data</button>
                 </div>
             </div>
         </div>
@@ -171,8 +170,17 @@
 
                     filteredImages.forEach(image => {
                         $('#fish-images').append(`
-                            <div>
-                                <img class="h-auto max-w-full rounded-lg" src="/data/images/${image.image}" alt="Fish Image">
+                            <div class="relative max-w-xl mx-auto group">
+                                <img class="h-auto max-w-full rounded-lg object-cover rounded-md" src="/data/images/${image.image}" alt="Fish Image">
+                                <div class="group-hover:visible invisible absolute inset-0 bg-gray-700 opacity-60 rounded-md"></div>
+                                <div class="group-hover:visible invisible absolute inset-0 flex items-center justify-center">
+                                    <button data-modal-target="delete-image-fish" data-modal-toggle="delete-image-fish" class="flex gap-1 item-center text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                        <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.625 1.53125V2.625H11.5938C11.7678 2.625 11.9347 2.69414 12.0578 2.81721C12.1809 2.94028 12.25 3.1072 12.25 3.28125C12.25 3.4553 12.1809 3.62222 12.0578 3.74529C11.9347 3.86836 11.7678 3.9375 11.5938 3.9375H2.40625C2.2322 3.9375 2.06528 3.86836 1.94221 3.74529C1.81914 3.62222 1.75 3.4553 1.75 3.28125C1.75 3.1072 1.81914 2.94028 1.94221 2.81721C2.06528 2.69414 2.2322 2.625 2.40625 2.625H4.375V1.53125C4.375 0.686 5.061 0 5.90625 0H8.09375C8.939 0 9.625 0.686 9.625 1.53125ZM3.934 5.84063L4.5115 11.6156C4.51693 11.6696 4.54223 11.7197 4.58249 11.7561C4.62276 11.7924 4.67511 11.8126 4.72938 11.8125H9.27063C9.32489 11.8126 9.37724 11.7924 9.41751 11.7561C9.45777 11.7197 9.48307 11.6696 9.4885 11.6156L10.066 5.84063C10.0877 5.67112 10.1747 5.51676 10.3084 5.41037C10.4421 5.30398 10.6121 5.25396 10.7822 5.27094C10.9522 5.28793 11.1089 5.37057 11.219 5.5013C11.329 5.63204 11.3838 5.80055 11.3715 5.971L10.794 11.746C10.7562 12.1238 10.5795 12.4741 10.298 12.729C10.0165 12.9838 9.65033 13.1249 9.27063 13.125H4.72938C4.34981 13.125 3.98379 12.9839 3.70231 12.7293C3.42083 12.4746 3.24396 12.1245 3.206 11.7469L2.6285 5.97187C2.61754 5.88499 2.62408 5.79679 2.64774 5.71247C2.6714 5.62815 2.7117 5.54942 2.76626 5.48091C2.82081 5.41241 2.88853 5.35552 2.96542 5.31359C3.04231 5.27167 3.12681 5.24556 3.21395 5.2368C3.30108 5.22804 3.38909 5.2368 3.47278 5.26259C3.55648 5.28837 3.63417 5.33064 3.70127 5.38691C3.76837 5.44318 3.82354 5.51232 3.86351 5.59024C3.90347 5.66816 3.92744 5.7533 3.934 5.84063ZM5.6875 1.53125V2.625H8.3125V1.53125C8.3125 1.47323 8.28945 1.41759 8.24843 1.37657C8.20741 1.33555 8.15177 1.3125 8.09375 1.3125H5.90625C5.84823 1.3125 5.79259 1.33555 5.75157 1.37657C5.71055 1.41759 5.6875 1.47323 5.6875 1.53125Z" fill="white"/>
+                                        </svg>
+                                        <span>Delete</span>
+                                    </button>
+                                </div>
                             </div>
                         `);
                     });
